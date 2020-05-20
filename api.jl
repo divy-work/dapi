8b464d9fc1fbd496cf36ed06be2e31c86c3bf7db
Divy Srivastava <dj.srivastava23@gmail,com>
2020-05-20 16:34:15 +0530

    initial commit :rocket:

---
+++ api.jl
@@ -0,0 +1,21 @@
+using Genie, Genie.Router
+using Genie.Renderer, Genie.Renderer.Html, Genie.Renderer.Json
+import YAML
+
+basic_data = YAML.load(open("data/basic.yml"))
+
+function get_cpu_stats()
+    st = read("/proc/stat", String)
+    m = match(r"^cpu *(.*)", st)
+    return parse.(Int, split(m.captures[1], ' '))
+end
+
+route("/data") do
+  json(basic_data)
+end
+
+route("/") do
+  html("status: up. CPU: ", get_cpu_stats())
+end
+
+up(8000, async = false)
